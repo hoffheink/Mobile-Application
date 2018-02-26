4e3853828f5f2115d2f7cb1d52c9dd32d9e246a5
Nicholas Sosinski <sosinskin@uwplatt.edu>
2018-02-26 09:40:08 -0600

    Fixing scoping issues

--- app/src/main/java/edu/uwplatt/projects1/spbmobile/CloudDatasource.java
+++ app/src/main/java/edu/uwplatt/projects1/spbmobile/CloudDatasource.java
@@ -26,6 +26,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+
 import edu.uwplatt.projects1.spbmobile.Appliance.Appliance;
 
 
@@ -48,9 +49,9 @@ public class CloudDatasource {
         US_EAST_2
     }
 
-    static List<Appliance> applianceList = new ArrayList<>();
+    public static List<Appliance> applianceList = new ArrayList<>();
 
-    static CloudDatasource getInstance(@NonNull Context inContext, @NonNull GoogleSignInAccount account, RegionEnum inRegion) {
+    public static CloudDatasource getInstance(@NonNull Context inContext, @NonNull GoogleSignInAccount account, RegionEnum inRegion) {
         if (ourInstance == null || !ourContext.equals(inContext) || !ourRegion.equals(inRegion)) {
             ourInstance = new CloudDatasource(inContext, inRegion);
             ourContext = inContext;
@@ -117,24 +118,24 @@ public class CloudDatasource {
                 listThingsRequest.setRequestCredentials(credentials);
 
                 try {
-                    List<String> thingNames = new ArrayList<>();
+                    /*List<String> thingNames = new ArrayList<>();
                     for (Policy policy : awsIot.listPrincipalPolicies(listPrincipalPoliciesRequest).getPolicies()) {
                         String policyName = policy.getPolicyName().replace("app-", "");
                         thingNames.add(policyName);
-                    }
+                    }*/
                     for (ThingAttribute o : awsIot.listThings(listThingsRequest).getThings()) {
-                        if (thingNames.contains(o.getThingName())) {
-                            Appliance appliance = new Appliance(o.getThingName(), o.getVersion().toString());
-                            String thingType = o.getThingTypeName();
-                            if (thingType != null) {
-                                switch (o.getThingTypeName()) {
-                                    case "coffee-maker":
-                                        appliance.setApplianceType(Appliance.ApplianceType.CoffeeMaker);
-                                        break;
-                                }
+                        //if (thingNames.contains(o.getThingName())) {
+                        Appliance appliance = new Appliance(o.getThingName(), o.getVersion().toString());
+                        String thingType = o.getThingTypeName();
+                        if (thingType != null) {
+                            switch (o.getThingTypeName()) {
+                                case "coffee-maker":
+                                    appliance.setApplianceType(Appliance.ApplianceType.CoffeeMaker);
+                                    break;
                             }
-                            newApplianceList.add(appliance);
                         }
+                        newApplianceList.add(appliance);
+                        //}
                     }
                 } catch (Exception e) {
                     Log.e("GetAppliancesRunnable", e.getMessage(), e);
