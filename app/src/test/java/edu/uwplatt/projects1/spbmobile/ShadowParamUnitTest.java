1b32a6dffa11bf8e4b0ad7d27fd3bceb318d8f47
hoffheink <password>
2018-03-22 11:43:24 -0500

    Made some changes to the test, and marked notes about how to improve the current code.

--- app/src/test/java/edu/uwplatt/projects1/spbmobile/ShadowParamUnitTest.java
+++ app/src/test/java/edu/uwplatt/projects1/spbmobile/ShadowParamUnitTest.java
@@ -12,24 +12,29 @@ public class ShadowParamUnitTest
 
     /**
      * Test requires manual comparison.
+     *
+     * TEST TO BE REMOVED!!!
      */
     @Test
     public void armCommandParams_SingleStateChange_ManualFormatTesting_TestPass()
     {
-        String TIMMY = "{\"mobileDeviceType\":\"timmy\",\"mobileDeviceVersion\":\"TimMy\",\"state\":{\"desired\":{\"tImmy\":\"tIMMY\"}},\"utcSendTime\":\"2017-12-04 20:33:39.852\"}";
-        String tIMMY = "timmy";
-        String timmy = "TimMy";
+        String CORRECT = "{\"mobileDeviceType\":\"Hat\",\"mobileDeviceVersion\":\"1.0.0\",\"state\":{\"desired\":{\"Bow\":\"on\"}},\"utcSendTime\":\"2017-12-04 20:33:39.852\"}";
+        String mType = "Hat";
+        String mVer = "1.0.0";
+        String Command = "Bow";
+        String newState = "on";
+
         ShadowParam shadowParam = new ShadowParam();
-        String TimMy = "tImmy";
-        String tImmy = "tIMMY";
-        String result = shadowParam.armCommandParams(tIMMY, timmy, TimMy, tImmy);
-        System.out.println(TAG + "Format should be:\n" + TIMMY);
+        String result = shadowParam.armCommandParams(mType, mVer, Command, newState);
+        System.out.println(TAG + "Format should be:\n" + CORRECT);
         System.out.println(TAG + "Format is:\n" + result);
-        assertNotEquals(TIMMY, result);
+        assertNotEquals(CORRECT, result);
     }
 
     /**
      * UNIMPLEMENTED TEST. TALK TO CLOUD/EMBED FOR VALIDITY.
+     *
+     * Implement test with the expected format.
      */
     @Test
     public void armCommandParams_MultiStateChange_ManualFormatTesting_TestPass()
