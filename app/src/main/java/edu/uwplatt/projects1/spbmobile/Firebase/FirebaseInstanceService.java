8946e6d12d5cee592d8c9724c1ff3495a7a52752
Kyle Hoffhein <hoffheink@uwplatt.edu>
2018-03-21 19:31:11 -0500

    Changes have been made.

--- app/src/main/java/edu/uwplatt/projects1/spbmobile/Firebase/FirebaseInstanceService.java
+++ app/src/main/java/edu/uwplatt/projects1/spbmobile/Firebase/FirebaseInstanceService.java
@@ -1,17 +1,13 @@
 package edu.uwplatt.projects1.spbmobile.Firebase;
 
 import android.util.Log;
-
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
-
-/**
- * Created by Bear on 3/19/2018.
- */
 
 public class FirebaseInstanceService extends FirebaseInstanceIdService
 {
     private final static String TAG = FirebaseInstanceService.class.getCanonicalName();
+
 
     @Override
     public void onTokenRefresh()
